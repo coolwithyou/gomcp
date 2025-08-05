96c7261d17fb061cce2c63196a47825dd5603c3a
coolwithyou <sw@ba-ton.kr>
2025-08-05 11:02:06 +0900

    chore: remove dist directory from git tracking
    - Remove cached dist files that should be ignored
    - Dist directory is already in .gitignore
    - These are build artifacts that shouldn't be in version control

--- dist/exports.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './types.js';
-export { servers, presets } from './servers.js';
-export { installServers, installPreset, verifyInstallations, listInstalledServers, removeServers, backupConfig, restoreConfig, } from './installer.js';
-export { type GomcpConfig, type InstalledServer, loadConfig, saveConfig, addInstalledServer, getInstalledServers, saveCustomPreset, getCustomPresets, } from './config.js';
-//# sourceMappingURL=exports.d.ts.map
