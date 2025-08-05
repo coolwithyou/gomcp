fb50ecf31b7391cb1672c46c158d2990da08085a
coolwithyou <sw@ba-ton.kr>
2025-08-05 21:10:35 +0900

    chore: remove dist directory from git tracking

--- dist/utils/git.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function getCurrentBranch(): Promise<string>;
-export declare function hasUncommittedChanges(): Promise<boolean>;
-export declare function getLatestTag(): Promise<string | undefined>;
-export declare function pushWithTags(): Promise<void>;
-export declare function isRepositoryClean(): Promise<boolean>;
-export declare function fetchRemote(): Promise<void>;
-export declare function isUpToDate(): Promise<boolean>;
-//# sourceMappingURL=git.d.ts.map
